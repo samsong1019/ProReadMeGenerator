2d59c79b949e38e6f040aea611cb9ec109631a2e
sam song <samuelsong18@gmail.com>
2022-09-22 20:12:22 -0400

    added packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkDown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = [];
 
